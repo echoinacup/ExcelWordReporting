cb9d2e5b0580d4e53580313e2a16e43ab958f140
echoinacup <ngkrychkov@gmail.com>
2018-11-05 22:26:42 +0100

    fix for formatting string

--- src/main/java/com/echoinacup/utils/HelpUtils.java
+++ src/main/java/com/echoinacup/utils/HelpUtils.java
@@ -1,14 +1,18 @@
 package com.echoinacup.utils;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.text.DecimalFormat;
 
 public class HelpUtils {
 
 
     public static String formatString(String value) {
-        double amount = Double.parseDouble(value);
+        double amount = 0;
         DecimalFormat formatter = new DecimalFormat("#,###");
-
+        if (StringUtils.isNotEmpty(value)) {
+            amount = Double.parseDouble(value);
+        }
         return formatter.format(amount);
     }
 }
